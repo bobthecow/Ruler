01e196164711af15ce2982924ffdbcb988a344d6
Justin Hileman <justin@justinhileman.info>
2012-05-17 13:58:01 -0700

    Switch to Composer for dependencies.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,20 +1,9 @@
 <?php
 
-require_once __DIR__ . '/../vendor/pimple/lib/Pimple.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-spl_autoload_register(function($class)
-{
-    if (0 === strpos($class, 'Ruler\\Test\\')) {
-        $file = __DIR__ . '/../tests/' . str_replace('\\', '/', $class) . '.php';
-        if (file_exists($file)) {
-            require_once $file;
-            return true;
-        }
-    } elseif (0 === strpos($class, 'Ruler\\')) {
-        $file = __DIR__ . '/../src/' . str_replace('\\', '/', $class) . '.php';
-        if (file_exists($file)) {
-            require_once $file;
-            return true;
-        }
-    }
+call_user_func(function() {
+    $loader = new \Composer\Autoload\ClassLoader();
+    $loader->add('Ruler\Test', __DIR__);
+    $loader->register();
 });
