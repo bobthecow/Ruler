3dd2a31e8c05e192b8077e8e5dc490fcf282a861
Enrico Baioni <enrico.baioni@zanui.com.au>
2013-10-23 16:21:01 +1100

    adds HasIntersection operator

--- src/Ruler/Value.php
+++ src/Ruler/Value.php
@@ -111,4 +111,15 @@ class Value
     {
         return $this->value < $value->getValue();
     }
+
+    /**
+     * @param Value $value
+     *
+     * @return boolean
+     */
+    public function hasIntersection(Value $value)
+    {
+        $result = array_intersect($this->value, $value->getValue());
+        return empty($result);
+    }
 }
