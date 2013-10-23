735a53fa8f7bb601594bde5c6e49837cd981b149
Enrico Baioni <enrico.baioni@zanui.com.au>
2013-10-23 16:28:24 +1100

    adds hasIntersection helper

--- src/Ruler/RuleBuilder/Variable.php
+++ src/Ruler/RuleBuilder/Variable.php
@@ -220,6 +220,18 @@ class Variable extends BaseVariable implements \ArrayAccess
     }
 
     /**
+     * Fluent interface helper to create a HasIntersection comparison operator.
+     *
+     * @param array $variable
+     *
+     * @return Operator\HasIntersection
+     */
+    public function hasIntersection($variable)
+    {
+        return new Operator\HasIntersection($this, $this->asVariable($variable));
+    }
+
+    /**
      * Private helper to retrieve a Variable instance for the given $variable.
      *
      * @param mixed $variable BaseVariable instance or value
