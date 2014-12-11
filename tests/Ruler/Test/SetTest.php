4d35578cb0a7c5fa717bd047546db433a3f3cca0
Jordan Raub <jordan@raub.me>
2014-12-10 21:44:56 -0800

    add more tests; add countable interface

--- tests/Ruler/Test/SetTest.php
+++ tests/Ruler/Test/SetTest.php
@@ -3,6 +3,8 @@
 namespace Ruler\Test;
 
 use Ruler\Set;
+use Ruler\Value;
+use Ruler\Test\Fixtures\toStringable;
 
 class SetTest extends \PHPUnit_Framework_TestCase
 {
@@ -13,6 +15,39 @@ class SetTest extends \PHPUnit_Framework_TestCase
             new \stdClass()
         );
         $set = new Set($setExpected);
-        $this->assertEquals(1, count($set));
+        $this->assertEquals(2, count($set));
+    }
+
+    public function testObjectUniqueness()
+    {
+        $objectA = new \stdClass();
+        $objectA->something = 'else';
+        $objectB = new \stdClass();
+        $objectB->foo = 'bar';
+
+        $set = new Set(array(
+            $objectA,
+            $objectB
+        ));
+
+        $this->assertEquals(2, count($set));
+        $this->assertTrue($set->setContains(new Value($objectA)));
+        $this->assertTrue($set->setContains(new Value($objectB)));
+        $this->assertFalse($set->setContains(new Value(false)));
+    }
+
+    public function testStringable()
+    {
+        $set = new Set(array(
+            $one = new toStringable(1),
+            $two = new toStringable(2),
+            $too = new toStringable(2)
+        ));
+
+        $this->assertEquals(2, count($set));
+        $this->assertTrue($set->setContains(new Value($one)));
+        $this->assertTrue($set->setContains(new Value($two)));
+        $this->assertTrue($set->setContains(new Value($too)));
+        $this->assertFalse($set->setContains(new Value(2)));
     }
 }
