906e3e3df6a6dc986ea792cbf85ed5299876d674
Justin Hileman <justin@justinhileman.info>
2022-06-26 14:46:44 -0400

    Update test bootstrap.

--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once 'vendor/autoload.php';
-
-if(class_exists('PHPUnit_Framework_Error_Deprecated')) {
-    class_alias('PHPUnit_Framework_Error_Deprecated', 'PHPUnit\Framework\Error\Deprecated');
-}
